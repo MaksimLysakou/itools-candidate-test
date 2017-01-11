d2403e1bf27576a379577b5a15d940657fd434d8
Xerotico <2008_96@mail.ru>
2017-01-11 18:14:18 +0300

    add config usage

--- server/app.js
+++ server/app.js
@@ -4,33 +4,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+
 const DAO = require('./dao');
 const authors = require('./routes/authors');
 const books = require('./routes/books');
-const config = require('./config');
 
-const Author = require('./models/author');
-
-const PORT = 3000;
-
-const DEBUG = true;
+const localConfig = require('./config');
 
 const app = express();
-const dao = new DAO();
+const dao = new DAO(localConfig.connection);
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/adminPage');
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-vm.db = mongoose.createConnection();
-vm.db.open(config.host, config.name, config.port);
-
-
-
-let test = new Author({ firstName : "a",
-                        secondName : "b",
-                        birthDate: new Date() });
 
 /**
  * Middleware
@@ -53,7 +36,7 @@ app.use('/api/books', books);
 /**
  * Init database
  */
-dao.init({/*init data*/}, (err, db) => {
+dao.init({/*init data*/}, (err) => {
 
     if (err) {
         console.error(err);
@@ -61,8 +44,8 @@ dao.init({/*init data*/}, (err, db) => {
     /**
      * Start app
      */
-    app.listen(PORT, function () {
-        console.log(`App listening on port ${PORT}!`);
+    app.listen(localConfig.application.port, function () {
+        console.log(`[INFO] : App listening on port ${localConfig.application.port}!`);
     });
 });
 
