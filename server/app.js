d2d280e36b11d822400c3c247795de01b2a9c301
Xerotico <2008_96@mail.ru>
2017-01-16 18:02:07 +0300

    add webpack commit

--- server/app.js
+++ server/app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('../webpack.config')
+
 
 const DAO = require('./dao');
 const authors = require('./routes/authors');
@@ -21,6 +26,9 @@ const dao = new DAO(localConfig.connection);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
 
 /**
