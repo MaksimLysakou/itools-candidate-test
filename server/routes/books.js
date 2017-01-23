13655306fc1ee3feaaf024d0aa5c8bf2aaeaea20
Xerotico <2008_96@mail.ru>
2017-01-23 17:14:36 +0300

    add routes

--- server/routes/books.js
+++ server/routes/books.js
@@ -5,7 +5,15 @@ const controller = require('../controllers/books');
 
 const router = express.Router();
 
-// TODO Add other book routes
-//router.get('/:id', controller.getBookById);
+router.get('/', controller.getBooks);
+router.get('/:id', controller.getBookById);
+
+router.put('/:id', controller.updateBook);
+
+router.patch('/:id', controller.editBook);
+
+router.post('/', controller.addBook);
+
+router.delete('/:id', controller.removeBook);
 
 module.exports = router;
