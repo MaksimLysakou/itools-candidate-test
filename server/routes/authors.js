09a251d754fe96aeace9b1211a587800c3998125
Xerotico <2008_96@mail.ru>
2017-01-23 17:13:03 +0300

    add routes

--- server/routes/authors.js
+++ server/routes/authors.js
@@ -5,10 +5,17 @@ const controller = require('../controllers/authors');
 
 const router = express.Router();
 
-// TODO Add other author routes
-
 router.get('/', controller.getAuthors);
 router.get('/:id', controller.getAuthorById);
+
+router.put('/:id', controller.updateAuthor);
+
+router.patch('/:id', controller.editAuthor);
+
+router.post('/', controller.addAuthor);
+
+router.delete('/:id', controller.removeAuthor);
+
 
 module.exports = router;
 
