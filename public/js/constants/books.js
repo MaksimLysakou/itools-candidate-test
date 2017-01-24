06d8244e7f1a77ec6ff5778edf4dab33b8c615db
Xerotico <2008_96@mail.ru>
2017-01-24 10:05:50 +0300

    add loading data from server

--- public/js/constants/books.js
+++ public/js/constants/books.js
@@ -1,2 +1,4 @@
 export const SAVE_BOOKS = 'SAVE_BOOKS';
 export const SET_BOOK_DIRTY = 'SET_BOOK_DIRTY';
+export const GET_BOOKS_REQUEST = 'GET_BOOKS_REQUEST'
+export const GET_BOOKS_SUCCESS = 'GET_BOOKS_SUCCESS'
--- public/js/store/configureStore.js
+++ public/js/store/configureStore.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
+import thunk from 'redux-thunk'
 
 export default function configureStore(initialState) {
-    return createStore(rootReducer, initialState);
+    return createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(thunk));
 }
